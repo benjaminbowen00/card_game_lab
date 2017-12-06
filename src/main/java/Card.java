473eee734bcbd99b901eccd8bda0d8a502470d05
benjaminbowen00 <benjibowen@hotmail.com>
2017-12-06 11:08:06 +0000

    card class setup

---
+++ src/main/java/Card.java
@@ -0,0 +1,21 @@
+public class Card {
+    private SuitType suit;
+    private ValueType value;
+
+    public Card(SuitType suit, ValueType value){
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public SuitType getSuit() {
+        return this.suit;
+    }
+
+    public ValueType getValue() {
+        return this.value;
+    }
+
+    public int getNumericalValue() {
+        return this.value.getValue();
+    }
+}
---
+++ src/test/java/CardTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+
+    Card card1;
+    Card card2;
+
+    @Before
+    public void before() {
+        card1 = new Card(SuitType.CLUBS, ValueType.SEVEN);
+        card2 = new Card(SuitType.SPADES, ValueType.QUEEN);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.CLUBS, card1.getSuit());
+    }
+
+}
